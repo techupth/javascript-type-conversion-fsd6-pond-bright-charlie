fafec932d966e07bf13d9d4ff7607e56356b5492
Sunbright53 <141810435+Sunbright53@users.noreply.github.com>
2024-05-18 02:18:31 +0700

    fix:pushing code of my section

--- ex-1.js
+++ ex-1.js
@@ -1,10 +1,8 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
-depositAmountFromUser = parseInt(depositAmountFromUser);
+accountBalance = accountBalance + Number(depositAmountFromUser);
 
-accountBalance = accountBalance + depositAmountFromUser;
-
-let accountBalanceMessage = "Your account balance is " + accountBalance;
+let accountBalanceMessage = `Your account balance is ${accountBalance}`;
 
 console.log(accountBalanceMessage);
